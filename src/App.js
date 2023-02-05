399eaad625a4a5149ffc832d6562fd1be11f0698
Sebastian <sebastian.stephenson1@gmail.com>
2023-02-05 01:10:26 -0500

    Rendered out the images for a test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import images from './images'
+import {images} from './images'
 function App() {
+
+  const test = images.map(element =>{
+    console.log(element.url)
+    return(
+      <img key={element.name} src={element.url} width='200px' height='200px'></img>
+    )
+});
+
+  console.log(images);
   return (
-   <></>
+   <>
+    {test}
+   </>
   );
 }
 
