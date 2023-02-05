ece78f3f57e9875e26b20e43425354d6355208b3
Sebastian <sebastian.stephenson1@gmail.com>
2023-02-05 00:32:03 -0500

    First commit

---
+++ src/images.js
@@ -0,0 +1,64 @@
+export const images = [
+    {
+        url: 'https://squishmallows.com/home/wp-content/uploads/2023/01/Squishmallows-12in-Conradina-Pink-Bigfoot-Heart-Headband-SQ22-WB-12BF-FRONT-lpr-removebg-preview.png',
+        name: 'Conradina',
+        desc: 'Conradina is an amazing writer. She sees the world with vivid imagery and emotion. Now she’s working up the courage to share her work. She wants to read in front of a crowd at open mic night, but public speaking makes her nervous. Can she practice reading a poem in front of you?',
+        desc2: 'Purple Bigfoot With Rainbow Hair Wearing Heart Headband With White Belly',
+        num: 1265,
+        date: '9.30.21'
+    },
+
+    {
+        url: 'https://squishmallows.com/home/wp-content/uploads/2022/10/SQ22-WB-12BP-12in-Black_Panther-Front-lpr-removebg-preview.png',
+        name: 'Sigzby',
+        desc: 'Meet Sigzby. This super special ‘Mallow is rarely seen in the wild, that’s because she’s nocturnal! When everyone else is sleeping, she’s up rock climbing, making origami, or practicing her ballet moves! When the sun comes up, you can find her snoozing in her favorite tree!',
+        desc2: 'Black Panther With Fuzzy Ears And Silver Spots',
+        num: 1302,
+        date: '9.21.21'
+    },
+
+    {
+        url: 'https://squishmallows.com/home/wp-content/uploads/2023/01/Squishmallows-12in-Wyatt-Laughing-Green-Frog-SQCR00235-FRONT.png',
+        name: 'Wyatt',
+        desc: 'Wyatt loves shooting hoops at home with his big sister, Wendy! These two ‘Mallows play pickup basketball all the way until dinner time. This year, Wyatt became a mathlete, so playing a fun game is just what he needs to stay sharp in between competitions.',
+        desc2: 'Green laughing frog',
+        num: 1932,
+        date: '8.24.22'
+    },
+
+    {
+        url: 'https://squishmallows.com/home/wp-content/uploads/2022/11/Squishmallows-12in-Lorenzo-Yellow-Banana-Slug-SQCR00233-FRONT-lpr-removebg-preview11.png',
+        name: 'Lorenzo',
+        desc: 'Lorenzo is a TV buff and blogger! He wants to use his knowledge and passion for television to break into showbiz. Lorenzo aspires to build his own TV network so he can schedule all his favorite shows and help create some new ones!',
+        desc2: 'Yellow Banana Slug',
+        num: 1602,
+        date: '3.30.22'
+    },
+
+    {
+        url: 'https://squishmallows.com/home/wp-content/uploads/2022/11/Squishmallows-12in-Winston-Teal-Owl-Fanny-Pack-SQCR00234-FRONT-lpr-removebg-preview22.png',
+        name: 'Winston',
+        desc: 'Winston is an aspiring chef who takes his cooking creativity to the next level. His friends inspire his latest culinary delights and he is always ready to whip up his famous mac and cheese.',
+        desc2: 'Teal Owl With Fanny pack',
+        num: 9210,
+        date: '6.28.22'
+    },
+
+    {
+        url: 'https://squishmallows.com/home/wp-content/uploads/2022/08/Foster.png',
+        name: 'Foster',
+        desc: 'Foster finds importance even in life’s little moments. He loves to celebrate any achievement, big or small, and made himself a promise to do exactly that in 2022! Even a get-together with his squad is celebration worthy and Foster is so grateful for each and every one of them.',
+        desc2: 'Black and White French Bulldog with Gold 2022 Embroidery on White Belly',
+        num: 1260,
+        date: '9.25.21'
+    },
+
+    {
+        url: 'https://squishmallows.com/home/wp-content/uploads/2022/10/SQUISHMALLOWS-12in-PURPLE-GRADIENT-AXELOTL-SQ22-WB-removebg-preview.png',
+        name: 'Aldea',
+        desc: 'This earthy mama is a holistic healer. She’s a certified Reiki massage therapist and she loves how much it heals. Aldea also knows all about plants and their healing property. Have a headache? She has the perfect essential oil for you!',
+        desc2: 'Teal and Purple Gradient Axolotl with White Belly',
+        num: 1264,
+        date: '9.29.21'
+    },
+]
